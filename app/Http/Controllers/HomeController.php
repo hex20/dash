5f7c27d0aa4db47f5f256f54a94a6c461de4af55
Angus Mckay <mckayangus3@gmail.com>
2015-12-20 01:22:52 +1300

    404 and db stuff

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,7 +10,7 @@ use DB;
 class HomeController extends Controller {
 
     public function index() {
-        $data = DB::table('price')->get();
+        $data = DB::table('price')->offset(45)->get(30);
         dd($data);
         return view('index');
     }
