399529f5b429a577c224783f7462eba6ff52f6a9
Angus Mckay <mckayangus3@gmail.com>
2015-12-20 00:29:00 +1300

    404 and db stuff

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700' rel='stylesheet' type='text/css'>
+    <title>Dash | Error 404</title>
+    <style>
+        body {
+            background-image: url("/./new_year_background.png");
+        }
+
+        h1, h2 {
+            font-weight: 300;
+            text-align: center;
+            font-family: 'Roboto', sans-serif;
+        }
+
+        h1 {
+            font-size: 40px;
+        }
+
+        img {
+            margin-left: auto;
+            margin-right: auto;
+            display: block;
+        }
+    </style>
+</head>
+<body>
+<h1>Error 404 page not found</h1>
+
+<h2>Mr. Simpson, this government computer can process over 9 tax returns per day. Did you really think you could fool
+    it?</h2>
+<img src="http://i.imgur.com/vfSGatq.jpg" alt="dash = no tax">
+</body>
+</html>
